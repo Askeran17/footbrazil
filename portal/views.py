ab5b0fb702abc4821425b6ce83a7506bb8d9ca6c
Askeran17 Atayev <m.ataev@mail.ru>
2024-05-08 15:52:29 +0000

    add theme bootstrap, add code to views.py

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import View
+from .models import Post
+
 # Create your views here.
 
-def my_portal(request):
-    return HttpResponse("Footbrazil Ole!")
+class PostView(View):
+    '''show posts'''
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'templates/base.html', {'post_list': posts})
+        paginate_by = 6
