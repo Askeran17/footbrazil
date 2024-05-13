7de42ef1f1c9b4027b353b4ddf4697d7aead16a5
Askeran17 Atayev <m.ataev@mail.ru>
2024-05-13 18:42:16 +0000

    fixed detailed pages to open them

--- portal/views.py
+++ portal/views.py
@@ -11,3 +11,9 @@ class PostView(View):
         posts = Post.objects.all()
         return render(request, 'portal/index.html', {'post_list': posts})
         paginate_by = 6
+
+class PostDetail(View):
+    '''full detailed posts'''
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        return render(request, 'portal/full_post.html', {'post': post})
