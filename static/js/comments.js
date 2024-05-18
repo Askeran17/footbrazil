a7adc5f341f72ec93e1f361ceeb93e2c97d0afc9
Askeran17 Atayev <m.ataev@mail.ru>
2024-05-18 15:30:46 +0000

    fixed edit comments in template

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,5 +1,5 @@
 const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_body");
+const commentText = document.getElementById("id_text_comments");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
