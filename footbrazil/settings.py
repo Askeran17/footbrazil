47e3d903fdb757c27932fd715b134b61da137484
Askeran17 <m.ataev@mail.ru>
2025-12-01 19:16:57 +0100

    fix css

--- footbrazil/settings.py
+++ footbrazil/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on (i.e True) in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-askeran17-footbrazil-ssyki954cga.ws-eu120.gitpod.io','.herokuapp.com', '.onrender.com']
 
